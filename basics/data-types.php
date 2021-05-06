ac30ba181d958766a9be8b6263009940568a63ff
Sahil Athia <sahilathia918@gmail.com>
2021-05-05 20:47:18 -0400

    php strings and integer

---
+++ basics/data-types.php
@@ -0,0 +1,14 @@
+<?php
+// strings
+
+$x = "Hello world!";
+$y = 'Hello world!';
+
+echo $x . "\n";
+echo $y . "\n";
+
+$x = 5985;
+
+// var dump returns data type and value
+var_dump($x) . "\n";
+?>
