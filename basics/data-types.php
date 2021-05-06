3279b79a93f66d2ee6441f8927e21a1b8624cc54
Sahil Athia <sahilathia918@gmail.com>
2021-05-05 20:50:04 -0400

    php float and boolean

--- basics/data-types.php
+++ basics/data-types.php
@@ -1,14 +1,32 @@
 <?php
-// strings
 
+// strings
 $x = "Hello world!";
 $y = 'Hello world!';
 
 echo $x . "\n";
 echo $y . "\n";
 
+// integer
 $x = 5985;
 
 // var dump returns data type and value
 var_dump($x) . "\n";
+
+// float
+// a number with a decimal
+
+$flt = 10.453;
+
+var_dump($flt) . "\n";
+
+
+// boolean
+
+$t = true;
+$f = false;
+
+var_dump($t) . "\n";
+var_dump($f) . "\n";
+
 ?>
